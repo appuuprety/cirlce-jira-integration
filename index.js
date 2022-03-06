bc9e0ae5bd1241165844c75fc096613b87c4a693
appuuprety <avishekuprety@gmail.com>
2022-03-05 17:14:37 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var app = express();
+
+// Root URL (/)
+app.get("/", function (req, res) {
+  res.send("Sample Jira App");
+});
+//server listening on port 8080
+app.listen(8080, function () {
+  console.log("Listening on port 8080!");
+});
